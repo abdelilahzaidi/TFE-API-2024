fdc36167ea387ec7013039c9a6bde8ddd2f77cb6
Abdelilah Zaidi <abdelilahzaidi1983@gmail.com>
2024-05-15 16:12:07 +0200

    add dto for auth : loginDTO and SignupDTO

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,11 @@
+import { UserService } from './../user/user.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        private userService : UserService,
+        private jwtService 
+    ){}
+}
---
+++ src/modules/auth/dto/signup.dto.ts
@@ -0,0 +1,56 @@
+import { IsNotEmpty, IsEmail, IsIn, IsBoolean, MinLength } from "class-validator";
+import { UserGender } from "src/common/enums/gender.enum";
+import { UserStatus } from "src/common/enums/status.enum";
+
+export class SignUpDTO {
+    @IsNotEmpty()
+    first_name: string;
+  
+    @IsNotEmpty()
+    last_name: string;
+  
+    @IsNotEmpty()
+    @IsEmail({}, { message: 'Please enter correct email address' })
+    email: string;
+  
+    @MinLength(6,{ message : 'Please enter minimum 6 character'})
+    @IsNotEmpty()
+    password: string;
+    
+    @MinLength(6,{ message : 'Please enter minimum 6 character'})
+    @IsNotEmpty()  
+    password_confirm: string;
+  
+    @IsNotEmpty()
+    @IsIn(['member', 'admin','responsale'], { message: 'Invalid status' }) 
+    status: UserStatus;
+  
+    @IsNotEmpty()
+    @IsIn(['male', 'female'], { message: 'Invalid gender' }) 
+    gender: UserGender
+  
+    @IsNotEmpty()
+      grade:string;
+  
+    @IsNotEmpty()
+    birthDate: Date;
+  
+    @IsNotEmpty()
+    rue: string;
+  
+    @IsNotEmpty()
+    commune: string;
+  
+    @IsNotEmpty()
+    ville: string;
+  
+    @IsNotEmpty()
+    @IsBoolean()
+    actif: boolean;
+  
+    @IsNotEmpty()
+    gsm: string;
+    constructor() {
+      this.status = UserStatus.MEMBER; 
+  }
+  }
